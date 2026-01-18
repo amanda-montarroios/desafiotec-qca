3864010d7ac17d6e7f326f6e3eb959b0e4d4e348
amanda-montarroios <amontarroiosoliveira@gmail.com>
2026-01-18 12:36:40 -0300

    analytics

---
+++ analytics.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+class InvoiceAnalytics:
+    def __init__(self, path: str = "database.json"):
+        self.df = pd.read_json(path)
+
+        self.items_df = self.df.explode("items")
+        self.items_df = pd.concat(
+            [self.items_df.drop(["items"], axis=1),
+             self.items_df["items"].apply(pd.Series)],
+            axis=1
+        )
+
+        self.items_df["total"] = (
+            self.items_df["quantity"] * self.items_df["unit_price"]
+        )
+
+    def media_invoice_valor(self):
+        return self.df.apply(
+            lambda x: sum(i["quantity"] * i["unit_price"] for i in x["items"]),
+            axis=1
+        ).mean()
+
+    def mais_frequente_produto(self):
+        return self.items_df["product_name"].value_counts().idxmax()
+
+    def total_gasto_por_produto(self):
+        return self.items_df.groupby("product_name")["total"].sum()
+
+    def produto_list(self):
+        return self.items_df[["product_name", "unit_price"]].drop_duplicates()
